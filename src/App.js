1701d886566d1db5ec32270dce0a2715d3e6fe69
abdulloh19 <parij4320@gmail.com>
2023-04-05 14:46:03 +0500

    added counters add

--- src/App.js
+++ src/App.js
@@ -1,48 +1,40 @@
-import { Component } from "react";
-import "./App.css";
+// import { Component } from "react";
+// import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello World</h1>
-//     </div>
-//   );
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       counter: 0,
+//       decrement: 1,
+//     };
+//   }
+
+//   counter = () => {
+//     const a = this.state.counter;
+//     this.setState({ counter: a + this.state.decrement });
+//     // this.setState((prevState) => ({ counter: prevState.counter + 1 }));
+//   };
+
+//   decrement = () => {
+//     const b = this.state.decrement;
+//     this.setState({ decrement: b + 1 });
+//     // this.setState((prevState) => ({ decrement: prevState.decrement + 1 }));
+//   };
+
+//   render() {
+//     // const { counter, decrement } = this.state;
+//     return (
+//       <div className="wrapper">
+//         <p className="counter">{this.state.counter} Metr</p>
+//         <button onClick={this.counter}>Yurish</button>
+//         <div>  
+//           <p className="counter">Qadamni kattaligi: {this.state.decrement}</p>
+//           <button onClick={this.decrement}>Qadamni kengaytirish</button>
+//         </div>
+//       </div>
+//     );
+//   }
 // }
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0,
-      decrement: 1,
-    };
-  }
-
-  counter = () => {
-    const a = this.state.counter;
-    this.setState({ counter: a + this.state.decrement });
-    // this.setState((prevState) => ({ counter: prevState.counter + 1 }));
-  };
-
-  decrement = () => {
-    const b = this.state.decrement;
-    this.setState({ decrement: b + 1 });
-    // this.setState((prevState) => ({ decrement: prevState.decrement + 1 }));
-  };
-
-  render() {
-    // const { counter, decrement } = this.state;
-    return (
-      <div>
-        <p className="counter">{this.state.counter} Metr</p>
-        <button onClick={this.counter}>Yurish</button>
-        <div>
-          <p className="counter">Qadamni kattaligi: {this.state.decrement}</p>
-          <button onClick={this.decrement}>Qadamni kengaytirish</button>
-        </div>
-      </div>
-    );
-  }
-}
-
-export default App;
+// export default App;
--- src/counter.js
+++ src/counter.js
@@ -1,24 +1,71 @@
-// import React, { useState } from "react";
+import React, { Component } from "react";
+import "./counter.css";
+import { prettyFormat } from "@testing-library/react";
 
-// function counter() {
-//   const [qadam1, setQadam] = useState([0]);
-//   const [metr, setMetr] = useState([0]);
-//   const counter = () => {
-//     setMetr(metr + 1);
-//   };
-//   const qadamlar = () => {
-//     setQadam(qadam1 * metr)
-//   }
-//   return (
-//     <div>
-//       <p className="counter">{qadam1} Metr</p>
-//       <button>Yurish</button>
-//       <div>
-//         <p className="counter">Qadamni kattaligi: {metr}</p>
-//         <button onClick={counter()}>Qadamni kengaytirish</button>
-//       </div>
-//     </div>
-//   );
-// }
+class Counter extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     addCounter: 0,
+  //     counter: 0,
+  //   };
+  // }
 
-// export default counter;
+  state = { counters: [] };
+
+  addCounter = () => {
+    const a = this.state.counters;
+    a.push(0);
+    this.setState({ counters: a });
+
+    // this.setState({ addCounter: a + this.state.counter });
+  };
+
+  counter = (index) => {
+    let b = this.state.counters;
+    b[index] = b[index] + 1;
+
+    this.setState({ counters: b });
+    // this.setState({ counter: b + 1 });
+  };
+  decrement = (index) => {
+    let b = this.state.counters;
+    b[index] = b[index] - 1;
+
+    this.setState({ counters: b });
+    // this.setState({ counter: c - 1 });
+  };
+
+  restart = () => {
+    this.setState({ counters: 0 });
+  };
+
+  render() {
+    return (
+      <div className="wr">
+        <button className="btn-add" onClick={this.addCounter}>
+          Add Counter
+        </button>
+        {this.state.counters.map(
+          (item, index) => (
+            <div className="counters">
+              <button onClick={() => this.counter(index)} className="btn">
+                +
+              </button>
+              {/* <p>{this.state.counters}</p> */}
+              {item}
+              <button onClick={() => this.decrement(index)} className="btn">
+                -
+              </button>
+            </div>
+          )
+          // <button onClick={() => this.restart(index)} className="res">
+          //   Restart
+          // </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Counter;
